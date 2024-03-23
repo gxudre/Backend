ccd0e13895d99e60d77555013422931fed8878b5
unknown <2214290016@ACADEMICO.IESB.BR>
2024-03-22 21:55:21 -0300

    adicionando testes aula06

--- aulas/aula06/app.js
+++ aulas/aula06/app.js
@@ -1,10 +1,8 @@
 var express = require('express');
-
-
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-const produtosRouter =  require("./routes/produtos")
+const produtosRouter = require('./routes/produtos');
 
 var app = express();
 
@@ -13,6 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/produtos', produtosRouter)
+app.use('/produtos', produtosRouter);
 
 module.exports = app;
--- aulas/aula06/controllers/controller_produto.js
+++ aulas/aula06/controllers/controller_produto.js
@@ -1,62 +1,60 @@
-const produtos = []
+const produtos = [];
 
 function listarTodos(req, res) {
-    res.json(produtos)
+    res.json(produtos);
 }
 
 function exibir(req, res) {
-    const { produtoEncontrado } = req
-    res.json(produtoEncontrado)
+    const { produtoEncontrado } = req;
+    res.json(produtoEncontrado);
 }
 
 function buscarPeloId(req, res, next) {
-    const { produtoId } = req.params
-
-    const produtoEncontrado = produtos.find(item => item.id == produtoId)
-
+    const { produtoId } = req.params;
+    const produtoEncontrado = produtos.find(item => item.id == produtoId);
     if (produtoEncontrado) {
-        req.produtoEncontrado = produtoEncontrado
-        next()
+        req.produtoEncontrado = produtoEncontrado;
+        next();
     } else {
-        res.status(404).json({ msg: "Produto não encontrado" })
+        res.status(404).json({ msg: "Produto não encontrado" });
     }
+}
 
+function validarDados(req, res, next) {
+    const { nome, preco } = req.body;
+    if (nome && preco) {
+        next();
+    } else {
+        res.status(422).json({ msg: "Nome e preço são obrigatórios" });
+    }
 }
 
 function criar(req, res) {
-    const { nome, preco } = req.body
-
-    const produtoNovo = {
-        id: produtos.length + 1,
-        nome: nome,
-        preco: preco
-    }
-
-    produtos.push(produtoNovo)
-
-    res.status(201).json(produtoNovo)
+    const { nome, preco } = req.body;
+    const produtoNovo = { id: produtos.length + 1, nome, preco }
+    produtos.push(produtoNovo);
+    res.status(201).json(produtoNovo);
 }
 
-function atualizar(req, res,) {
-    const { produtoId } = req.params
-    const { nome, preco } = req.body
-
-    const { produtoEncontrado } = req
-
-    produtoEncontrado.nome = nome
-    produtoEncontrado.preco = preco
-    res.json(produtoEncontrado)
-
+function atualizar(req, res) {
+    const { nome, preco } = req.body;
+    const { produtoEncontrado } = req;
+    produtoEncontrado.nome = nome;
+    produtoEncontrado.preco = preco;
+    res.json(produtoEncontrado);
 }
 
 function remover(req, res) {
-    const { produtoId } = req.params
-    const posicao = produtos.findIndex(item => item.id == produtoId)
-
+    const { produtoId } = req.params;
+    const posicao = produtos.findIndex(item => item.id == produtoId);
     if (posicao >= 0) {
-        produtos.splice(posicao, 1)
-        res.status(204).end()
+        produtos.splice(posicao, 1);
+        res.status(204).end();
     }
 }
 
-module.exports = { listarTodos, buscarPeloId, criar, atualizar, remover, exibir }
+module.exports = {
+    listarTodos, exibir, buscarPeloId, validarDados,
+    criar, atualizar, remover
+}
+
