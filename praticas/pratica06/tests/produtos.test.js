bb540ad2ea81afb2aed5cd08f7d1b271fcf40989
Gustavo Xudré <gxudre@gmail.com>
2024-03-26 20:44:51 -0300

    Adicionando pratica 06

--- praticas/pratica06/tests/produtos.test.js
+++ praticas/pratica06/tests/produtos.test.js
@@ -1,51 +1,45 @@
-const supertest = require('supertest');
-
-const app = require('../app');
-
-const request = supertest(app);
+const supertest = require("supertest");
+const app = require("../app")
 
 
+const request = supertest(app)
 
-describe('teste da API Produtos', function (){
-    
-    test("POST / deve retornar 201 um array JSON", async function () {
-        const novo = {nome: "uva", preco: 18.0};
+describe("teste da api produtos", function() {
+
+    test("POST / deve retornar 201 um array JSON", async function(){
+        const novo = { nome: "uva", preco: "20.0"};
         const response = await request.post("/produtos").send(novo);
-        expect (response.status).toBe(201);
-        expect (response.type).toBe("application/json");
-        expect (response.body).toHaveProperty("id");
-        expect (response.body).toHaveProperty("nome", novo.nome);
-        expect (response.body).toHaveProperty("preco", novo.preco);
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id",1);
+        expect(response.body).toHaveProperty("nome", novo.nome);
+        expect(response.body).toHaveProperty("preco", novo.preco);
     });
 
-    test("POST / deve retornar 422 um objeto JSON", async function () {
+    test("POST /produtos sem um JSON e retorna o status 422 e um conteúdo do tipo JSON ", async function(){
         const response = await request.post("/produtos");
         expect (response.status).toBe(422);
         expect (response.type).toBe("application/json");
         expect (response.body).toHaveProperty("msg", "Nome e preço são obrigatórios");
     });
 
-    test("GET /id deve retornar 200 um array JSON", async function () {
+    test("GET / deve retornar 200 e um conteúdo do tipo JSON contendo um array de objetos", async function(){
         const response = await request.get("/produtos");
-        expect (response.status).toBe(200);
-        expect (response.type).toBe("application/json");
+        expect(response.status).toBe(200);
         expect(Array.isArray(response.body)).toBe(true);
     });
 
-    test("GET /id deve retornar 200 um objeto JSON", async function () {
+    test("GET /produtos/1 retorna o status 200 e um conteúdo do tipo JSON", async function(){
         const response = await request.get("/produtos/1");
-        expect (response.status).toBe(200);
-        expect (response.type).toBe("application/json");
-        expect (response.body).toHaveProperty("id");
-        expect (response.body).toHaveProperty("nome");
-        expect (response.body).toHaveProperty("preco");
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("id",1);
+        expect(response.body).toHaveProperty("nome");
+        expect(response.body).toHaveProperty("preco");
     });
 
-    test("GET /id deve retornar 404 um objeto JSON", async function () {
+    test("GET /produtos/100 retorna o status 404  e um conteúdo do tipo JSON", async function(){
         const response = await request.get("/produtos/100");
-        expect (response.status).toBe(404);
-        expect (response.type).toBe("application/json");
-        expect (response.body).toHaveProperty("msg", "Produto não encontrado");
+        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty("msg", "Produto não encontrado");
     });
 
     test("PUT /id deve retornar 200 um objeto JSON", async function () {
@@ -56,13 +50,6 @@ describe('teste da API Produtos', function (){
         expect (response.body).toHaveProperty("id");
         expect (response.body).toHaveProperty("nome", atual.nome);
         expect (response.body).toHaveProperty("preco", atual.preco);
-    });
-
-    test("PUT /id deve retornar 422 um objeto JSON", async function () {
-        const response = await request.put("/produtos/1");
-        expect (response.status).toBe(422);
-        expect (response.type).toBe("application/json");
-        expect (response.body).toHaveProperty("msg", "Nome e preço são obrigatórios");
     });
 
     test("PUT /id deve retornar 404 um array JSON", async function () {
@@ -86,4 +73,5 @@ describe('teste da API Produtos', function (){
         expect (response.body).toHaveProperty("msg", "Produto não encontrado");
     });
 
-});
+
+})
